7f357544b253f431d8d28c02deaa767c50982323
Matteo <grecomatteo86@gmail.com>
2021-02-26 16:50:25 +0100

    sconto del 20% minorenni commit

--- js/script.js
+++ js/script.js
@@ -6,4 +6,8 @@ var eta = prompt('quanti anni hai?');
 
 //3. calcolo prezzo biglietto in base ai km (0.21 € al km)
 var prezzo = km * 0.21;
-console.log(prezzo);
+
+//4. sconto del 20% per i minorenni
+if (eta < 18) {
+  alert('il tuo biglietto costa ' + ((prezzo * 20) / 100 ) + '€');
+}
