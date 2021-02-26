5d455eb556bbd5706748e08d4e5f1ab6a323b925
Matteo <grecomatteo86@gmail.com>
2021-02-26 19:36:54 +0100

    Intro parseInt

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 //1.chiedo all'utente il numero di km che intende percorrere
-var km = prompt('quanti km vuoi percorrere?');
+var km = parseInt(prompt('quanti km vuoi percorrere?'));
 
 //2. chiedo all'utente la sua età
-var eta = prompt('quanti anni hai?');
+var eta = parseInt(prompt('quanti anni hai?'));
 
 //3. calcolo prezzo biglietto in base ai km (0.21 € al km)
 var prezzo = km * 0.21;
