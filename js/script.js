dcc7bcda478fb959ef381bc824bdc547ade3309b
Matteo <grecomatteo86@gmail.com>
2021-02-26 16:15:50 +0100

    prompt_km_età commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//1.chiedo all'utente il numero di km che intende percorrere
+var km = prompt('quanti km vuoi percorrere?');
+
+//2. chiedo all'utente la sua età
+var eta = prompt('quanti anni hai?');
