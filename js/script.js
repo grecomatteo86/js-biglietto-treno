eac4fe5dd6c4fab79a176f18f3b62c9fb9c210da
Matteo <grecomatteo86@gmail.com>
2021-02-26 16:34:14 +0100

    calcolo prezzo biglietto commit

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ var km = prompt('quanti km vuoi percorrere?');
 
 //2. chiedo all'utente la sua età
 var eta = prompt('quanti anni hai?');
+
+//3. calcolo prezzo biglietto in base ai km (0.21 € al km)
+var prezzo = km * 0.21;
+console.log(prezzo);
